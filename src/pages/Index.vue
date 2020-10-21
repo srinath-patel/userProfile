de8dc43e767340db0353205caed2d1127514a925
Adithya <adithya.bhupathiraju@gmail.com>
2020-10-21 19:52:07 +0530

    Inital Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card class="my-card">
+      <q-card-section class="bg-purple text-white">
+        <div class="text-h6">User Login</div>
+      </q-card-section>
+
+     
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
